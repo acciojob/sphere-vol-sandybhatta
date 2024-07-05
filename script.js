fe8c42adcc403969e218a502e6a94ac581d1b07b
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-07-05 21:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function volume_sphere() {
     //Write your code here
+	let rad=document.querySelector("#radius");
+	let vol=document.querySelector("#volume");
+	let form=document.querySelector("form");
+	form.addEventListener("submit",(e)=>{
+		e.preventDefault();
+		let radValue=rad.value;
+		let flag=false;
+		for(let val=0;val<radValue.length;val++){
+			let ascii= radValue.charCodeAt(val);
+			if(ascii>=48 && ascii <=57){
+				flag=true;
+				continue;
+			}
+			else{
+				flag=false;
+				break;
+			}
+			
+		}
+		if(flag==true){
+			let volume=(4/3)*Math.PI*radValue**3;
+			// volume=volume.tofixed(4);
+			volume=Math.round(volume*10000)/10000
+			vol.value=volume
+		}
+		else{
+			vol.value="NaN"
+		}
+	})
   
 } 
 
